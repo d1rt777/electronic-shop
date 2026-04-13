a3572c5290a6ea6e46bd3633171ef81e97589034
d1rt777 <sarkozz161@gmail.com>
2026-04-13 10:33:46 +0300

    Перенос проекта на php

---
+++ admin/data.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../scripts/connect.php';
+
+$name_product = $_POST['name'];
+$price_product = $_POST['price'];
+$description_product = $_POST['description'];
+$image_product = $_FILES['image'];
+
+$newFileName = $image_product['name'];
+
+move_uploaded_file($image_product['tmp_name'], "../img/$newFileName");
+
+$connect->query("INSERT INTO `products`(`name`, `price`, `description`, `photo`) 
+VALUES ('$name_product','$price_product','$description_product','$newFileName')");
+
+echo 'все получилось';
+
+?>
